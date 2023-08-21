d5ad4dc1f6dce334efa8480323698c55daa2c635
MattiasIngelstrom <mattias.ingelstrom@iea.lth.se>
2023-08-21 16:44:01 +0200

    Test

---
+++ src/test/java/org/matsim/project/RunMatsimTestExample.java
@@ -0,0 +1,17 @@
+package org.matsim.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RunMatsimTestExample {
+
+    @Test
+    public void testSomething() {
+
+        Assert.assertEquals(2, 1 + 1);
+
+    }
+
+}
